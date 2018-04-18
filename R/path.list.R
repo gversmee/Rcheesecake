add9e94a7bcd1a139227552b6a1324bbb4885817
gversmee <34459351+gversmee@users.noreply.github.com>
2018-04-18 11:02:30 -0400

    debug

--- R/path.list.R
+++ R/path.list.R
@@ -15,10 +15,12 @@ path.list <- function(env, var, token, verbose = FALSE) {
     path <- content.get(paste0(env, "/rest/v1/resourceService/find?term=", gsub("\\*", "%", basename(e))), token)
     path <- as.character(sapply(path, "[", 1))
     if (dirname(e) != ".")  path <- path[grepl(URLencode(dirname(e), reserved = TRUE), sapply(path, URLencode, reserved = TRUE))]
-    return(path)
+    if (!is.null(path))  return(path)  else return(NULL)
   })
 
-  if (!is.null(pathlist))  {
+  pathlist <- unlist(pathlist)
+
+  if (length(pathlist) != 0)  {
 
     if (verbose)  sapply(pathlist, message)
     return(unlist(pathlist))
